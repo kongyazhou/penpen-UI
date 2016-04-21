d2359ebc3c45e87ec7b80115a42b6e81bf422718
淡淡的秋天 <416229293@qq.com>
2016-04-21 14:51:33 +0800

    添加gulp-htmlmin
    进一步工程化

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
+var htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', function() {
-  gulp.src('js/*.js')
-  .pipe(ngAnnotate())
-  .pipe(concat('penpen.bundle.min.js'))
-  .pipe(uglify())  
-  .pipe(gulp.dest('www/js'));
+gulp.task('default', ['js', 'html']);
+
+gulp.task('js', function() {
+    gulp.src('js/*.js')
+        .pipe(ngAnnotate())
+        .pipe(concat('penpen.bundle.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('www/js'));
+});
+
+gulp.task('html', function() {
+    gulp.src('html/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+        .pipe(gulp.dest('www/templates'));
 });
